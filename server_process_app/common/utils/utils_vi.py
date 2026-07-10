41c52de92fd463e005472318dd61443af1974dd3
mquevedo001 <martinqpmo01@gmail.com>
2026-07-10 09:36:04 +0200

    alarms taxonomy imports bug 2

--- server_process_app/common/utils/utils_vi.py
+++ server_process_app/common/utils/utils_vi.py
@@ -13,6 +13,7 @@ RELATIVE_PATH_YAMNET_MAP = config['models']['yamnet_class_map']
 
 RELATIVE_PATH_TAXONOMY_PORT = config['models']['urban_taxonomy_mapping']
 RELATIVE_PATH_TAXONOMY_URBAN = config['models']['port_taxonomy_mapping']
+
 def collect_df_server(reg_folder):
 
 
@@ -288,20 +289,20 @@ def remove_unnamed_columns(df_preds):
 
 
 def yamnet_class_map_csv():
-    home_dir = os.path.expanduser('~')
-    yammnet_class_map_path = os.path.join(home_dir, RELATIVE_PATH_YAMNET_MAP.lstrip('/'))
+    
+    yammnet_class_map_path = RELATIVE_PATH_YAMNET_MAP
     df_audioset = pd.read_csv(yammnet_class_map_path,sep=';')
     df_audioset = remove_unnamed_columns(df_audioset)
     return df_audioset
 
 
 def taxonomy_json():
-    home_dir = os.path.expanduser('~')
-    urban_taxonomy_map_path = os.path.join(home_dir, RELATIVE_PATH_TAXONOMY_URBAN.lstrip('/'))
+    
+    urban_taxonomy_map_path = RELATIVE_PATH_TAXONOMY_URBAN
     urban_taxonomy_map = pd.read_json(urban_taxonomy_map_path, typ='series').to_dict()
     
     
-    port_taxonomy_map_path = os.path.join(home_dir, RELATIVE_PATH_TAXONOMY_PORT.lstrip('/'))
+    port_taxonomy_map_path = RELATIVE_PATH_TAXONOMY_PORT
     port_taxonomy_map = pd.read_json(port_taxonomy_map_path, typ='series').to_dict()
     return urban_taxonomy_map, port_taxonomy_map
 
